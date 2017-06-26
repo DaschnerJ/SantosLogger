7e87aae883178dbbc6320b991aed3fa083d98e5a
justdasc <justdasc@WC-2FJ4SY1TL.performance.pfgc.com>
2017-06-26 11:22:11 -0400

    Added in comments to explain code and reasoning. Removed redundant code within Log file.

--- src/santoslogger/gui/Log.java
+++ src/santoslogger/gui/Log.java
@@ -5,7 +5,6 @@
  */
 package santoslogger.gui;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.logging.FileHandler;
 import java.util.logging.Level;
@@ -23,15 +22,35 @@ public class Log {
     //The extension to be used for the log files.
     private final String extension = ".satlog";
 
-    //
+    //Path location of where to put the file.
     private String location;
+    /**
+     * The name of the file. This is separate in order to be able
+     * to create the directory and the file if they do not exist.
+     */
     private String name;
+    /**
+     * The java logger used to print the information to the log
+     * file. This may be replaced at a later date with a new logger
+     * in order to be able to have more control over the format and
+     * headers with in the log file.
+     */
     private Logger logger;
+    /**
+     * The frame for GUI for the logger.
+     */
     private final JFrame g;
+    
+    //Optional setting to see exit codes displayed.
     private Boolean exitCode = false;
     
+    //File manager from Santos Utils to assist in file and directory creation.
     private final FileManager f = new FileManager();
 
+    /**
+     * Creates a file with the given name in the default logs directory.
+     * @param name Name of the file.
+     */
     public Log(String name) {
         this.location = f.GetExecutionPath() +"logs\\";
         this.name = name;
@@ -41,6 +60,11 @@ public class Log {
         g.setTitle(name);
     }
     
+    /**
+     * Creates a file with the given name in the default logs directory.
+     * @param name Name of the file.
+     * @param exitCode Optional parameter to display exit code.
+     */
     public Log(String name, Boolean exitCode) {
         this.location = f.GetExecutionPath() +"logs\\";
         this.name = name;
@@ -51,6 +75,11 @@ public class Log {
         g.setTitle(name);
     }
     
+    /**
+     * Creates a log file at the specified location with the specified name.
+     * @param name Name of the log file.
+     * @param location Directory location of the file.
+     */
     public Log(String name, String location) {
         this.location = location;
         this.name = name;
@@ -60,6 +89,12 @@ public class Log {
         g.setTitle(name);
     }
     
+    /**
+     * Creates a log file at the specified location with the specified name.
+     * @param name Name of the log file.
+     * @param location Directory location of the file.
+     * @param exitCode Optional parameter to display exit codes.
+     */
     public Log(String name, String location, Boolean exitCode)
     {
         this.location = location;
@@ -71,6 +106,9 @@ public class Log {
         g.setTitle(name);
     }
     
+    /**
+     * Allows for toggling the logging of exit codes on and off.
+     */
     public void toggleExitCode()
     {
         exitCode = !exitCode;
@@ -80,14 +118,30 @@ public class Log {
             addInfo("Command exit codes will no longer be displayed.");
     }
 
+    /**
+     * Changes the file name to start logging to. This method is a setter
+     * and is not meant to be used.
+     * @param name Name of the new file. 
+     */
+    @Deprecated
     public void changeName(String name) {
         this.name = name;
     }
 
+    /**
+     * Changes the log file location. This method is a setter and is not
+     * meant to be used.
+     * @param location The new location of the file.
+     */
+    @Deprecated
     public void changeLocaiton(String location) {
         this.location = location;
     }
 
+    /**
+     * Adds a normal info entry to the Santos Log.
+     * @param text The text to be inputed into the entry.
+     */
     public void addInfo(String text) {
         logger.info(text);
         if (GUI.console.getText().equals("") || GUI.console == null) {
@@ -97,6 +151,10 @@ public class Log {
         }
     }
 
+    /**
+     * Adds a warning entry to the Santos Log.
+     * @param text The text to be inputed into the entry.
+     */
     public void addWarning(String text) {
         logger.warning(text);
         if (GUI.console.getText().equals("") || GUI.console == null) {
@@ -106,6 +164,10 @@ public class Log {
         }
     }
 
+    /**
+     * Adds a severe entry to the Santos Log.
+     * @param text The text to be inputed into the entry.
+     */
     public void addSevere(String text) {
         logger.severe(text);
         if (GUI.console.getText().equals("") || GUI.console == null) {
@@ -115,6 +177,10 @@ public class Log {
         }
     }
 
+    /**
+     * Adds a fine entry to the Santos Log.
+     * @param text The text to be inputed into the entry.
+     */
     public void addFine(String text) {
         logger.fine(text);
         if (GUI.console.getText().equals("") || GUI.console == null) {
@@ -124,6 +190,10 @@ public class Log {
         }
     }
 
+    /**
+     * Adds a finest entry to the Santos Log.
+     * @param text The text to be inputed into the entry.
+     */
     public void addFinest(String text) {
         logger.finest(text);
         if (GUI.console.getText().equals("") || GUI.console == null) {
@@ -133,6 +203,9 @@ public class Log {
         }
     }
 
+    /**
+     * Makes the log GUI visible or invisible to the user.
+     */
     public void showLog() {
         if (g.isVisible()) {
             g.setVisible(false);
@@ -141,29 +214,57 @@ public class Log {
         }
     }
 
+    /**
+     * Creates the initial file to be able to log to.
+     */
     private void createLog() {
         FileHandler fh;
         logger = Logger.getLogger(name);
         try {
+            /**
+             * Creates initial directory and log as noted in the Santos Utils
+             * create.
+             */
             f.createDirectory(location);
             f.createFile(location + name + extension);
-            File f = new File(location + name + extension);
-            // This block configure the logger with handler and formatter  
+            
+            /**
+             * Here we use a file handler to be able to create a new log
+             * to start appending to. We use a simple formatter to enable
+             * the log to be read as HTML. The reason for this is to allow
+             * color text and special formatting with the log for later
+             * updates.
+             */
             fh = new FileHandler(location + name + extension);
             logger.addHandler(fh);
             SimpleFormatter formatter = new SimpleFormatter();
             fh.setFormatter(formatter);
 
-            // the following statement is used to log any messages  
+            //Log that the log has been created for reference. 
             logger.info("Santos Log has been created.");
 
         } catch (SecurityException | IOException e) {
+            //Print out that the log failed to create.
             Logger.getLogger(Log.class.getName()).log(Level.SEVERE, null, e);
         }
     }
 
+    /**
+     * Executes the command in a new thread and prints the results to the
+     * Santos Logger log.
+     * @param command The command to be executed.
+     */
     public void executeCommand(String command) {
+        /**
+         * Creates a CommandThread to execute the command. The reason for
+         * threading the commands is to allow simultaneous commands to
+         * be ran at once and the program will not be held up by waiting
+         * for a single command to be executed.
+         */
         CommandThread cmd = new CommandThread(command, this, exitCode);
+        /**
+         * This is separate for security and thread stability reasons.
+         */
         cmd.start();
     }
 }
